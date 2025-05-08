828a92c0ec7d1603def60f5f8752a1a15db419f6
marcos.almeida <me@marcosalmeida.dev>
2025-05-08 22:17:59 +0200

    Add messages

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,8 +14,15 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "me@marcosalmeida.dev", body: "Hi"),
+        Message(sender: "me2@marcosalmeida.dev", body: "Hello"),
+        Message(sender: "me@marcosalmeida.dev", body: "Bye")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         navigationItem.hidesBackButton = true
         navigationItem.title = K.appName
     }
@@ -33,4 +40,18 @@ class ChatViewController: UIViewController {
         }
     }
     
+}
+
+//MARK: - UITableViewDataSource
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
+    }
 }
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Marcos Almeida on 2025-05-08.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
